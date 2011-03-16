19a79f8d77db71666131d8ff9a18c072c469139f
Erich Ocean <erich.ocean@me.com>
2011-03-16 03:12:19 -0700

    cleaned up main.js

--- apps/dashboard/main.js
+++ apps/dashboard/main.js
@@ -11,13 +11,13 @@
 // As you develop your application you will probably want to override this.
 // See comments for some pointers on what to do next.
 //
-Dashboard.main = function main() {
+function main() {
 
   // Step 1: Instantiate Your Views
   // The default code here will make the mainPane for your application visible
   // on screen.  If you app gets any level of complexity, you will probably 
   // create multiple pages and panes.  
-  Dashboard.getPath('mainPage.mainPane').append() ;
+  Dashboard.mainPage.get('mainPane').append() ;
 
   // Step 2. Set the content property on your primary controller.
   // This will make your app come alive!
@@ -25,6 +25,4 @@ Dashboard.main = function main() {
   // TODO: Set the content property on your primary controller
   // ex: Dashboard.contactsController.set('content',Dashboard.contacts);
 
-} ;
-
-function main() { Dashboard.main(); }
+};
