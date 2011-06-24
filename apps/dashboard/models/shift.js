7f722c1a16f3d4bfef9560cf7a4a5934575c3541
Erich Ocean <erich.ocean@me.com>
2011-06-24 16:20:10 -0700

    fixed coloring to match latest spec, and update_date/updated_time fields

--- apps/dashboard/models/shift.js
+++ apps/dashboard/models/shift.js
@@ -16,21 +16,35 @@ Dashboard.Shift = SC.Object.extend(
   /** @scope Dashboard.Shift.prototype */ {
 
   status: function() {
-    var age, updatedAt = this.get('updatedAt');
+    var age =  this.get('shiftAt') - Dashboard.get('updatedAt'),
+        updatedAt = this.get('updatedAt');
 
-    if (updatedAt) {
-      age = Dashboard.get('updatedAt') - updatedAt;
-    } else {
-      age = Dashboard.get('updatedAt') - this.get('callTaken');
+    // we return a color if the shifts starts in the next 48 hours
+    if (age < FOURTY_EIGHT_HOURS) {
+      if (!updatedAt) {
+        // if we've never updated, we turn green in the first 30 minutes,
+        if (age > (FOURTY_EIGHT_HOURS - THIRTY_MINUTES)) return 1; // green
+        // and red thereafter, unless
+        else return 2; // red
+      // we've updated the system, in which case we return green if it's been less than 30 minutens since we updated,
+      } else if ((Dashboard.get('updatedAt') - updatedAt) < THIRTY_MINUTES) return 1; // green
+      // and red otherwise
+      else return 2; // red
+      // shifts starting more than 48 hours from now show up as white
     }
-
-    if (age < THIRTY_MINUTES)          return 2;
-    else if (age < FOURTY_EIGHT_HOURS) return 1;
-    else                               return 0;
+    // otherwise, we return white
+    else return 0; // white
   }.property('callTaken').cacheable(),
 
+  shiftAt: function() {
+    var dateTime = this.roster_date+' '+this.start_time.slice(0,-8)+'+10:00';
+    return SC.DateTime.parse(dateTime, '%Y-%m-%d %H:%M:%S%Z')._ms;
+  }.property(),
+
   updatedAt: function() {
-    var dateTime = this.call_taken_date+' '+this.call_taken_time.slice(0,-8)+'+10:00';
+    if (!this.updated_date || !this.updated_time) return 0;
+
+    var dateTime = this.updated_date+' '+this.updated_time.slice(0,-8)+'+10:00';
     return SC.DateTime.parse(dateTime, '%Y-%m-%d %H:%M:%S%Z')._ms;
   }.property(),
 
